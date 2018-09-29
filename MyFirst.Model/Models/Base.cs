669e86b06ab9eef465ced53d2b65ea373daa9666
Alvin <alvin.vafana@wonga.com>
2018-09-29 23:59:38 +0200

    first commit

---
+++ MyFirst.Model/Models/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirst.Model.Models
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ MyFirst.Model/Models/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirst.Model.Models
+{
+    public class Store:Base
+    {
+        public string Name { get; set; }
+        public int NumberOfProducts { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+    }
+}
